d676422f042142ba5db6310d9cd24ccbcc5eb2e0
BioWalker <39074222+shijianasdf@users.noreply.github.com>
2019-04-30 08:39:55 +0800

    Create github.R

---
+++ github.R
@@ -0,0 +1,12 @@
+#github中检索
+#survival AND strata  language:R
+#coxph AND stepwise language:R
+#library(mice) OR library(mi)  language:R
+#D3.js AND data visualization language:JavaScript
+
+
+#google scholar检索具有代码的文章
+#(survival) AND "https://github"
+#(logist) AND "https://github"
+#(RNA-seq OR RNAseq) AND (ceRNA) AND "https://github"
+#(SNP6.0) AND "https://github"
