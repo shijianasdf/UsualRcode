cad6cb8316c9334f6eb468f05fb13dd47268dba6
BioWalker <39074222+shijianasdf@users.noreply.github.com>
2025-09-03 14:51:16 +0800

    Update install.R

--- install.R
+++ install.R
@@ -2,7 +2,7 @@
 options()$repos 
 options()$BioC_mirror
 options(BioC_mirror="https://mirrors.ustc.edu.cn/bioc/")
-options(BioC_mirror="http://mirrors.tuna.tsinghua.edu.cn/bioconductor/")
+options(BioC_mirror="https://mirrors.tuna.tsinghua.edu.cn/bioconductor/")
 options("repos" = c(CRAN="https://mirrors.tuna.tsinghua.edu.cn/CRAN/"))
 options()$repos 
 options()$BioC_mirror
