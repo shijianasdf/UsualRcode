c730c11d5c970d28fdef05511ef27f4960f80e5c
BioWalker <39074222+shijianasdf@users.noreply.github.com>
2026-01-11 19:48:09 +0800

    Update install.R

--- install.R
+++ install.R
@@ -36,7 +36,7 @@ devtools::install_github("aertslab/SCopeLoomR")
 devtools::install_github('mojaveazure/loomR')
 devtools::install_github('smorabit/hdWGCNA', ref='dev')
 devtools::install_github("velocyto-team/velocyto.R")
-
+remotes::install_github('cailab-tamu/scTenifoldKnk')
 #下载到服务器，然后用install_local()来安装
 devtools::install_local("PATH/TO/DIRECTORY/CytoTRACE_0.3.3.tar.gz")
 # https://bioconductor.org/packages/release/bioc/html/GEOquery.html
